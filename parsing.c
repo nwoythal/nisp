9c04178e123a4e6f0bb29e57dda8d620b229b13e
Nate W <nwoythal@gmail.com>
2015-11-26 02:27:49 -0700

    added language & grammar

--- parsing.c
+++ parsing.c
@@ -37,17 +37,39 @@ void add_history(char * unused){} //Empty add_history function since windows
 
 int main(int argc, char** argv)
 {
-    puts("Nisp pre-alpha\nctrl+c to exit\n");
+    
+    //Language & Grammar
     mpc_parser_t* Number   = mpc_new("number");
     mpc_parser_t* Operator = mpc_new("operator");
     mpc_parser_t* Expr     = mpc_new("expr");
     mpc_parser_t* Lispy    = mpc_new("lispy");
+    mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+        number   : /-?[0-9]+/;              \
+        operator : '+' | '-' | '*' | '/' | '%' | /add/ | /sub/ | /mul/ | /div/ | /mod/; \
+        expr     : <number> | '(' <operator> <expr>+ ')'; \
+        lispy    : /^/ <operator> <expr>+ /$/;            \
+    ",
+    Number, Operator, Expr, Lispy);
+    //| '/-?[0-9].[0-9]+/'
+    puts("Nisp pre-alpha\nctrl+c to exit\n");
     while(TRUE)
     {
         char * input=readline("> ");
         add_history(input); //Add to history buffer
-        printf("%s\n" ,input);
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispy, &r))
+        {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        }
+        else
+        {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
         free(input); //de-allocate
     }
+    mpc_cleanup(4,Number,Operator,Expr,Lispy);
     return 0;
 }
